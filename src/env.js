6e2b63efef1a7011f55bebc12383be16d33ea3fd
meng.wm <meng.wm@alibaba-inc.com>
2019-01-13 14:25:50 +0800

    init project

---
+++ src/env.js
@@ -0,0 +1,35 @@
+// Browser environment sniffing
+export const inBrowser = typeof window !== 'undefined'
+export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
+export const isIE = UA && /msie|trident/.test(UA)
+export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
+export const isEdge = UA && UA.indexOf('edge/') > 0
+export const isAndroid = (UA && UA.indexOf('android') > 0)
+export const isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA))
+export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
+
+// detect if browser support passive event 
+// which is used to improve scoll behavior in mobile device
+export let supportsPassive = false
+if (inBrowser) {
+  try {
+    const opts = {}
+    Object.defineProperty(opts, 'passive', ({
+      get () {
+        supportsPassive = true
+      }
+    }))  
+    window.addEventListener('test-passive', null, opts)
+// The above expression is NOT aync, and event name can be anything else 
+// If browser supports passive event, it will try to get opts's passive property
+// Otherwise, it will treat opts as boolean.  so the getter of opts are never touched 
+  } catch (e) {}
+}
+
+export function isNative (Ctor) {
+  return typeof Ctor === 'function' && /native code/.test(Ctor.toString())
+}
+
+export const hasSymbol =
+  typeof Symbol !== 'undefined' && isNative(Symbol) &&
+  typeof Reflect !== 'undefined' && isNative(Reflect.ownKeys)
